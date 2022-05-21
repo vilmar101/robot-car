24ec2325190b9bb6254cae1731c88cc14af08d4a
vilmar101 <87516278+vilmar101@users.noreply.github.com>
2022-05-21 14:34:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+})
+basic.forever(function () {
+    basic.pause(20000)
+    music.playMelody("C D E F G A B C5 ", 2229)
+})
+basic.forever(function () {
+    basic.pause(420000)
+    music.playMelody("G B E C D F B A ", 2229)
 })
