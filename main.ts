742b1a477987f7819d563f74eef09d0148a9a9a9
vilmar101 <87516278+vilmar101@users.noreply.github.com>
2022-05-21 14:48:52 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,19 +5,10 @@ function Yellow_LED_lights_on () {
 	
 }
 function Red_LED_lights_on () {
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
 }
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-basic.forever(function () {
-    basic.showIcon(IconNames.SmallSquare)
-    basic.showIcon(IconNames.Square)
-})
-basic.forever(function () {
-    basic.pause(20000)
-    music.playMelody("C D E F G A B C5 ", 2229)
-})
-basic.forever(function () {
-    basic.pause(420000)
-    music.playMelody("G B E C D F B A ", 2229)
-})
