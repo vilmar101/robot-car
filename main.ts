d7e25c3c2411546cb12e8fbb9b6cd8fa0db767a8
vilmar101 <87516278+vilmar101@users.noreply.github.com>
2022-05-21 14:51:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 function Green_LED_lights_on () {
-	
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
 }
 function Yellow_LED_lights_on () {
-	
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
 }
 function Red_LED_lights_on () {
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
